8dce4877a61870c25676bbd91aec56bddf647bcc
Ritik Kumar <ritik.kumar006@gmail.com>
2021-02-27 15:19:38 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+// Connect Database
+
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("API running!");
+});
+
+app.use("/users", require("./routes/users"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || "5000";
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
