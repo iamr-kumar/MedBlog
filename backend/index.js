33939ecbc1a24315c03188bf65b91993c1dfae78
Ritik Kumar <ritik.kumar006@gmail.com>
2021-03-02 01:10:45 +0530

    prepare for deploy

--- backend/index.js
+++ backend/index.js
@@ -20,15 +20,7 @@ app.use("/auth", require("./routes/auth"));
 app.use("/posts", require("./routes/posts"));
 app.use("/doctors", require("./routes/doctor"));
 
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || "5000";
 
