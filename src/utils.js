9149e356b527e035fbc02bd9e9c94d63fa4e186c
Ritik <ritik.kumar006@gmail.com>
2022-01-10 01:13:14 +0530

    fix for deploy

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://medblogrestapi.herokuapp.com/"
+    ? "https://medblogrestapi.herokuapp.com"
     : "http://localhost:5000";
